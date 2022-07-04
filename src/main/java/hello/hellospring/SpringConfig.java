c21023160d2b81a0a226b8d1f46a09249d71cf5b
wnsvy607 <wnsvy607@naver.com>
2022-07-04 17:39:31 +0900

    시간측정 로직: 공통관심사항(cross-cutting concern) AOP없이 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,35 +2,33 @@ package hello.hellospring;
 
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    //    @Autowired   생성자가 1개일 경우 생략 가능하다.
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean   //Bean을 등록한다는 의미
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    스프링 JPA는 상속한 부모 클래스를 자동으로 매핑해준다. SpringDataJpaMemberRepositoy 인터페이스가
+//    MemberRepository를 상속받기 때문에, 해당 구현체를 만들어 위의 생성자에 매핑해준다.
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//
+//    }
 
 }
