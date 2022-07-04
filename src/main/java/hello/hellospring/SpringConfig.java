05771c70e8821a7e4aecc7ebdc0234445f70ab3f
wnsvy607 <wnsvy607@naver.com>
2022-07-05 03:47:09 +0900

    AOP 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,12 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+//    Aop는 컴포넌트 어노테이션을 이용해도 되고, 여기서 직접 Bean으로 등록해도 된다.
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
+//
 //    스프링 JPA는 상속한 부모 클래스를 자동으로 매핑해준다. SpringDataJpaMemberRepositoy 인터페이스가
 //    MemberRepository를 상속받기 때문에, 해당 구현체를 만들어 위의 생성자에 매핑해준다.
 //    @Bean
